518dcff4fbe8e882407437fe63da80283297d56a
Kaitao Chen <79537350+kxc663@users.noreply.github.com>
2023-04-21 00:49:24 -0400

    Update: add history lowest price, add price table

--- public/app.js
+++ public/app.js
@@ -1,3 +1,5 @@
+const amazonResultBaseUrl = "https://www.amazon.com/"
+
 $("#searchButton").click(async function () {
     const searchName = $("#searchInput").val().trim();
     if (searchName === "") {
@@ -10,8 +12,65 @@ $("#searchButton").click(async function () {
         } else if (response.error) {
             throw new Error(response.error);
         } else {
-            console.log(response);
+            generateTable(response);
+            console.log(response.length);
             console.log("success");
         }
     }
 });
+
+async function generateTable(dataSet) {
+    $("#displayTable").show();
+    console.log(dataSet);
+    const tableBody = document.getElementById("tableBody");
+    for (var i = 0; i < dataSet.length; i++) {
+        const row = document.createElement("tr");
+        const nameCell = document.createElement("td");
+        const name = dataSet[i][0];
+        nameCell.classList.add('product-name');
+        nameCell.innerHTML = `<a href="${amazonResultBaseUrl}${dataSet[i][1].url}">${name}</a>`;
+        row.appendChild(nameCell);
+        const imageCell = document.createElement("td");
+        const imageElement = document.createElement("img");
+        await loadImage(dataSet[i][1].image, imageElement);
+        imageElement.alt = "product image";
+        imageElement.width /= 2;
+        imageElement.height /= 2;
+        imageCell.appendChild(imageElement);
+        row.appendChild(imageCell);
+        const priceCell = document.createElement("td");
+        const price = removeExtraDollarSign(dataSet[i][1].price);
+        priceCell.innerHTML = price;
+        row.appendChild(priceCell);
+        const lowestPriceCell = document.createElement("td");
+        const lowestPrice = await $.get("/lowestPrice", { id: dataSet[i][1].id });
+        console.log(lowestPrice);
+        lowestPriceCell.innerHTML = lowestPrice;
+        row.appendChild(lowestPriceCell);
+        tableBody.appendChild(row);
+        await new Promise(resolve => setTimeout(resolve, 3000));
+    }
+}
+
+function removeExtraDollarSign(priceString) {
+    var dollarSignCount = 0;
+    for (var i = 0; i < priceString.length; i++) {
+        if (priceString[i] === "$") {
+            dollarSignCount++;
+        }
+    }
+    if (dollarSignCount > 1) {
+        const priceRegex = /\$([0-9]+,?[0-9]*\.[0-9]{2})/;
+        const priceMatch = priceString.match(priceRegex);
+        return priceMatch[0];
+    }
+    return priceString;
+}
+
+async function loadImage(url, elem) {
+    return new Promise((resolve, reject) => {
+        elem.onload = () => resolve(elem);
+        elem.onerror = reject;
+        elem.src = url;
+    });
+}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const port = 8000;
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { promises } = require('dns');
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,7 @@ const static_path = path.join(__dirname, "public");
 app.use(express.static(static_path));
 
 amazonBaseUrl = "https://www.amazon.com/s?k="
+camelBaseUrl = "https://camelcamelcamel.com/product/"
 
 app.get('/search', (req, res) => {
     const name = req.query.q;
@@ -19,27 +21,71 @@ app.get('/search', (req, res) => {
     // get the html from the amazon product url
     // parse the html to get the product name, picture, and price
     // return the product name, picture, and price
-    let html;
     axios.get(amazonProductUrl)
         .then(response => {
             const $ = cheerio.load(response.data);
+            const products = new Map();
             $('.sg-col-20-of-24.s-result-item.s-asin.sg-col-0-of-12.sg-col-16-of-20.sg-col.s-widget-spacing-small.sg-col-12-of-16').each((i, element) => {
                 const productName = $(element).find('span.a-size-medium').text();
                 const productPrice = $(element).find('span.a-price > span.a-offscreen').text()
                 const productImage = $(element).find('img.s-image').attr('src');
-                console.log('Product Name:', productName);
-                console.log('Product Price:', productPrice);
-                console.log('Product Image:', productImage);
+                const productID = $(element).attr('data-asin');
+                const productURL = $(element).find('a.a-link-normal.a-text-normal').attr('href');
+                products.set(productName, {
+                    price: productPrice,
+                    image: productImage,
+                    id: productID,
+                    url: productURL
+                });
             });
-            // use the $ object to manipulate the HTML
+            if(products.size === 0) {
+                $('.sg-col-4-of-24.sg-col-4-of-12.s-result-item.s-asin.sg-col-4-of-16.sg-col.s-widget-spacing-small.sg-col-4-of-20').each((i, element) => {
+                    const productName = $(element).find('span.a-size-base-plus').text();
+                    const productPrice = $(element).find('span.a-price > span.a-offscreen').text()
+                    const productImage = $(element).find('img.s-image').attr('src');
+                    const productID = $(element).attr('data-asin');
+                    const productURL = $(element).find('a.a-link-normal.a-text-normal').attr('href');
+                    products.set(productName, {
+                        price: productPrice,
+                        image: productImage,
+                        id: productID,
+                        url: productURL
+                    });
+                }); 
+            }
+            res.json([...products]);
         })
         .catch(error => {
             res.send("N/A");
             console.log(error);
         });
-
-    //res.send(amazonProductUrl);
 });
+
+app.get('/lowestPrice', async (req, res) => {
+    const productID = req.query.id;
+    const camelProductUrl = camelBaseUrl + productID;
+    axios.get(camelProductUrl)
+        .then(response => {
+            const $ = cheerio.load(response.data);
+            const price = $('.lowest_price').text();
+            res.send(extractPrice(price));
+        })
+        .catch(error => {
+            console.log(error);
+            res.send("N/A");
+        });
+});
+
+
+function extractPrice(str) {
+    const regex = /(\$[\d,]+\.\d+)/;
+    const match = regex.exec(str);
+    if (match) {
+        return match[1];
+    } else {
+        return null;
+    }
+}
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
